38db949af5aea5a45f7a0de648bf68f67f5a38a7
Jose Luis Puente <puenterodriguezjose@gmail.com>
2020-08-28 00:05:56 -0700

    Add Views

--- app.js
+++ app.js
@@ -40,18 +40,48 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 // RESTFUL ROUTES
 
-app.get("/", function(req, res){
+// INDEX ROUTES
+app.get("/", function (req, res) {
     res.redirect("/blogs");
 });
 
 app.get("/blogs", function (req, res) {
-    Blog.find({}, function(err, blogs){
-        if(err){
+    Blog.find({}, function (err, blogs) {
+        if (err) {
             console.log("ERROR!");
         } else {
-            res.render("index", {blogs: blogs});
+            res.render("index", { blogs: blogs });
         }
     });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function (req, res) {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function (req, res) {
+    // create blog
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            // then redirect to index
+            res.redirect("/blogs");
+        }
+    })
+})
+
+//  SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   })
 });
 
 app.listen(port, function () {
